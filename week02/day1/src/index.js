56f43bd8dde2ed047d939b2d7773e087b3cf7820
root <root@ixWorkLaptop>
2024-04-08 11:44:26 -0700

    week2 added

---
+++ week02/day1/src/index.js
@@ -0,0 +1,39 @@
+// What is a program?
+
+// A program runs top of the file to the bottom
+
+// Console.log logs print something to the console. 
+// In this case it will log it to our browser console
+
+
+//Datatypes *******************************
+//Primitive 
+// String : Text
+  
+// Boolean : True and False
+  //Comparison operators
+
+// Number: 1 2 3 4
+  //NaN
+  // Bigint.
+  
+// Undefined.
+  // not defined and undefined are not the same
+
+// Null.
+  // given a value of nothing
+
+// Symbol.
+// represents unique values and are used as identifiers or keys to objects.
+
+//Non-Primitive *******************
+// Object.
+
+// Arrays 
+
+
+//Variables **************************************************
+
+
+
+// Functions ********************************
