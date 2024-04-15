08eb88f303b8cadfbf94cc539a817c3a9a25c331
root <root@ixWorkLaptop>
2024-04-15 11:53:24 -0700

    adding further learning exercise

---
+++ week03/day1-futher-learning/src/index.js
@@ -0,0 +1,21 @@
+//Create a calculator
+
+// Create a function called myCalculator that takes a two numbers and an operator as params
+// Based on the operator, the function should perform the correct calculation and return the results
+
+// Create a function called renderToDom that takes two numbers, an operator, and calculation results as params.
+// Select an h2 element, and set its text content to the calculation and result
+// For example, 2 + 2 = 4
+
+//Create a main function and invoke it at the bottom of the file.
+// within the main function
+// Using prompt, ask the user for a number
+// Using prompt again, ask the user for an operator
+// Using prompt again, ask the user for another number
+//Call myCalculator and pass the prompts as arguments
+// Call renderToDom and pass it the prompts and return value from myCalculator as arguments
+
+// Test your code in the browser to see if it works.
+// Update your code:
+// Add a new prompt that asks the user for their favorite color
+// Create a function that grabs that h2 and updates its inline style color to match the user's favorite color. If the user's favorite color isn't one of the built in css colors, promp the user for the Hex code for their favorite color.
