f53753e29aadda3fb3c7d810b352cd6446214f2c
root <root@ixWorkLaptop>
2024-04-17 10:41:30 -0700

    adding week3

---
+++ week03/day-2-further-learning/src/index.js
@@ -0,0 +1,13 @@
+// Challenge 1
+// Using JavaScript, update text in the rainbow section of the html doc to match the color written. For example, the <li>Red</li> should be the color red in the browsers.
+
+// Challenge 2
+//Create a New Year Count Down Clock
+
+//1. Create a variable that selects the h3 element
+
+//Do not change any of the pre-written code
+// The Code written in this function will run every second
+setInterval(function () {
+  //2. Your Code in here: Render the Month, Weeks, Days, Hours, Minutes, and seconds until the new year.
+}, 1000);
---
+++ week03/day-2/src/index.js
@@ -0,0 +1,34 @@
+//Selecting elements
+
+//Change element
+
+//Traversing the Dom
+//Getting nested elements
+
+//TIME -
+//developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+//Current date
+
+//YYYY-MM-DDTHH:mm:ss.sssUTC
+//Year - Month - day T Hour : minute : second timezone
+//Coordinated Universal Time or UTC
+
+//Returns milliseconds
+//Technically don't need the getTime() here
+// 1000 milliseconds in a second
+// 60 seconds in a minute
+// 60 minutes in an hour
+// 24 hours in a day
+
+//Find the milliseconds remainder of timeLeft / days
+//Convert the reminder to Hours
+
+//Find the milliseconds remainder of timeLeft / hours
+//Convert the reminder to minutes
+
+//Find the milliseconds remainder of timeLeft / minutes
+//Convert the reminder to seconds
+
+//How to create a countdown Timer. (n.d.). https://www.w3schools.com/howto/howto_js_countdown.asp
+
+//Go over prompt for Breakout session
