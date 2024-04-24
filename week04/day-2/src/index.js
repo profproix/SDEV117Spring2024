d7049610578377054ae56cb56f3f91d7de897ad8
root <root@ixWorkLaptop>
2024-04-24 10:17:30 -0700

    week 4 day 2 starter

---
+++ week04/day-2/src/index.js
@@ -0,0 +1,10 @@
+//Function Review and Scope
+//This variable is in global scope
+
+//Function returns a discount price
+
+//APA citation for image
+//Roy, K. (2019, June 30). four multicolored ornaments. Unsplash. https://unsplash.com/photos/four-multicolored-ornaments-bYy9hndOx0k
+//"https://images.unsplash.com/photo-1561865406-62a037159577?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+//Call your other functions here
