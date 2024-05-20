7a841cfb6d2aeb6bd63909e97c54bc7fca9e890f
root <root@ixWorkLaptop>
2024-05-20 11:36:56 -0700

    breakout session

---
+++ week08/BreakoutDay1/src/index.js
@@ -0,0 +1,15 @@
+// Create DOM elements and render the following object to the DOM
+// The img should be in an image tag, but the rest of the dom elements are up to you.
+// Hint:
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+// https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+let luke = {
+  name: "Luke Skywalker",
+  height: "172",
+  mass: "77",
+  hair_color: "blond",
+  skin_color: "fair",
+  eye_color: "blue",
+  birth_year: "19BBY",
+  img: "https://static.wikia.nocookie.net/starwars/images/1/17/LukeSkywalker-Insider217.png/revision/latest?cb=20240130192944",
+};
